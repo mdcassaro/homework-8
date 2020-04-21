da6e9915a450298294a3782abfcc0cd30999151c
Michael Cassaro <mdcassaro@hotmail.com>
2020-04-21 18:36:05 -0500

    added necessary files

--- Develop/app.js
+++ Develop/app.js
@@ -12,11 +12,85 @@ const render = require("./lib/htmlRenderer");
 ​
 ​
 // Write code to use inquirer to gather information about the development team members,
+ inquirer
+ .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your Employees name?"
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your employee's GitHub username?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your employee's email address?"
+    },
+    {
+      type: "checkbox",
+      name: "job",
+      message: "What is your job?",
+      choices:[
+          "Engineer",
+          "Manager",
+          "Intern"
+
+      ]
+    },
+    
+  ])
+  .then(function(response){
+      if(response.job.choices === "Engineer"){
+          console.log ("Your employee is an Engineer")
+      }else if(response.job.choices === "Manager"){
+        console.log ("Your employee is a Manager")
+        } else {
+        console.log ("Your employee is an Engineer")
+    }
+
+  })
+
+
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+function Employee (name, username, email, job){
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.job = job
+}
 ​
+const newEmployee = new Employee(answers.name, answers.username, answers.email, answers.job)
+
+Employee.id = function(){
+    this.id++; 
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+function generateHTML(answers) {
+    return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">{{ name }}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ role }}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: {{ ${answers.id} }}</li>
+            <li class="list-group-item">Email: <a href="mailto:{{ ${answers.email} }}">{{ email }}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/{{ ${answers.username} }}" target="_blank" rel="noopener noreferrer">{{ github }}</a></li>
+        </ul>
+    </div>
+</div>`
+
+}
+
+
 ​
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -33,3 +107,22 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
+
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      const html = generateHTML(answers);
+  
+      await writeFileAsync("index.html", html);
+  
+      console.log("Successfully wrote to index.html");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
+  
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,3 +1,39 @@
 // TODO: Write code to define and export the Employee class
 
 //this is the main prototype all will share
+
+const inquirer = require("inquirer");
+
+
+ inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your Employees name?"
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your employee's GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your employee's email address?"
+      },
+      {
+        type: "checkbox",
+        name: "job",
+        message: "What is your job?",
+        choices:[
+            "Engineer",
+            "Manager",
+            "Intern"
+
+        ]
+      },
+      
+    ]);
+  
+
+  module.exports = Employee;
